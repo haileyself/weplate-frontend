fbabfc809366e0be3d7e48325d75c09adcf90414
koyoungnam <ynko62@gmail.com>
2019-09-02 10:28:20 +0900

    signup desktop screen

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,30 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Signup.scss';
 import Logo from './logo.png';
-import {Link} from 'react-router-dom';
+import {Link, withRouter } from 'react-router-dom';
 
-function Signup() {
-  return (
-    <div class="signup-page">  
-      <article class="signup-content">
-        <div class="input-wrap">
-          <div class="signup-header">
-              <Link to="/"><img class="signup-logo" src={Logo} alt="logo_img"></img></Link>
-          </div>
-          <div class="signup-info">
-            <div class="signup-title">Register.</div>
-            <div class="signup-subtitle">Enter your email to create an account.</div>
-          </div>
-          <div class="signup-form">
-            <input type="email" placeholder="Please enter your Email" maxLength="30" /> 
-            <input type="password" placeholder="Your Password" maxLength="8" /> 
-            <input type="password" placeholder="Your Password" maxLength="8" />
-            <input type="button" class="signup-btn" value="Register" />
-          </div>
+class Signup extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  clickLoginBtn = () => {
+    const {
+      email,
+      password,
+      history,
+    } = this.state;
+
+    if (email.length === 0) {
+      alert('Email을 입력해주세요');
+    }
+    if (password.length === 0) {
+      alert('Password를 입력해주세요');
+    }
+    history.push('/');
+  };
+
+    setEmail = (e) => {
+      this.setState({ email: e.target.value });
+    }
+
+    setPassword = (e) => {
+      this.setState({ password: e.target.value });
+    }
+
+    render() {
+      return (
+        <div className="signup-page">  
+          <article className="signup-content">
+            <div className="input-wrap">
+              <div className="signup-header">
+                  <Link to="/"><img class="signup-logo" src={Logo} alt="logo_img"></img></Link>
+              </div>
+              <div className="signup-info">
+                <div className="signup-title">Register.</div>
+                <div className="signup-subtitle">Enter your email to create an account.</div>
+              </div>
+              <div className="signup-form">
+                <input 
+                  onChange={this.setEmail} 
+                  type="email" 
+                  placeholder="Please enter your Email" 
+                  value={this.state.email}
+                  maxLength="30" 
+                /> 
+                <input 
+                  onChange={this.setPassword}
+                  type="password" 
+                  placeholder="Your Password" 
+                  value={this.state.password}
+                  maxLength="8" 
+                /> 
+                <input 
+                  onChange={this.setPassword}
+                  type="password" 
+                  placeholder="Your Password" 
+                  value={this.state.password}
+                  maxLength="8" 
+                />
+                <input type="button" className="signup-btn" value="Register"  onClick={this.clickLoginBtn} />
+              </div>
+            </div>
+          </article>
         </div>
-      </article>
-    </div>
-  );
+      );
+    }
 }
 
-export default Signup;
+export default withRouter(Signup);
