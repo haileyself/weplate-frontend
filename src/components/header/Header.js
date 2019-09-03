070f091a86b66cd51716079be793342163b6dcdf
khsi12345 <khsi6237@gmail.com>
2019-09-03 16:29:24 +0900

    발표 피드백 반영 및 완성도 작업 진행 중

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ShowMenu from './ShowMenu';
 import './header.scss';
@@ -6,43 +7,78 @@ import './header.scss';
 class Header extends PureComponent {
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
+    this.state = {
+      scroll: 0,
+      isLogin: false,
+    };
   }
 
-  token = localStorage.getItem('weple-token');
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.getTokenKey();
+  }
+
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    // console.log(window.scrollY);
+    this.setState({
+      scroll: window.scrollY,
+    });
+    this.myRef.current.className = this.state.scroll >= 480 ? 'header' : 'header header--Transparent';
+  }
+
+  getTokenKey = () => {
+    const token = localStorage.getItem('weple-token');
+    console.log(token);
+    if (token) {
+      // console.log('??');
+      this.setState((prev) => (
+        { isLogin: !prev.isLogin }
+        // console.log(!prev.isLogin)
+      ));
+    }
+    // console.log(token);
+  }
+
+  tokenHandler = () => {
+    localStorage.setItem('null', this.token);
+    this.setState((prev) => (
+      { isLogin: !prev.isLogin }
+    ));
+  }
+
 
   render() {
     const {
-      onClickHandlerMenuIcon, onClickEvent, onChangeEvent, mode,
+      onClickEvent, mode, logoOrange, logoWhite,
     } = this.props;
     console.log(this.token);
     return (
-      <header className="header">
+      <header ref={this.myRef} className="header header--Transparent">
         <a className="header_logo" href="/">
-          <i className="header_logo_logoIcon" alt="MANGOPLATE 로고 이미지" />
+          <img
+            src={this.state.scroll >= 480 ? logoOrange : logoWhite}
+            className="header_logo_logoIcon"
+            alt="MANGOPLATE 로고 이미지"
+          />
         </a>
-        <i className="search_icon" alt="전체 검색창 아이콘" />
-        <form className="form_wrap">
-          <fieldset className="fieldset_wrap">
-            <legend className="form_fieldset">전체 검색</legend>
-            <label htmlFor="mms" className="label_wrap">
-              <input
-                type="text"
-                id="mms"
-                placeholder="지역, 식당 또는 음식"
-                className="search_text"
-                onChange={onChangeEvent}
-              />
-              <button
-                type="button"
-                className="search_btn"
-                onClick={onClickHandlerMenuIcon}
-              >CLEAR
-              </button>
-
-
-            </label>
-          </fieldset>
-        </form>
+        <div className="header_searchBox">
+          <i className="header_searchIcon" alt="전체 검색창 아이콘" />
+          <label htmlFor="ms" className="heaer_searchInputWrap">
+            <input
+              type="text"
+              id="ms"
+              className="header_searchInput"
+              placeholder="지역, 식당 또는 음식"
+            />
+          </label>
+          <button type="submit" className="header_searchInputButton" />
+        </div>
         <ul className="header_menulist">
           <li className="header_menuitem underline-from-left">
             {/* <a className="header_menulink" href="/"> */}
@@ -63,27 +99,27 @@ class Header extends PureComponent {
               <span>망고스토리</span>
             </Link>
           </li>
-          <div className="header_menuitem_wrap ">
-            {this.token ? (
-              <li className="header_menuitem underline-from-left">
-                <Link to="./login">
-                  <span>로그아웃</span>
-                </Link>
-              </li>
-            )
-              : (
-                <li className="header_menuitem underline-from-left">
-                  <Link to="./login">
-                    <span>로그인</span>
-                  </Link>
-                </li>
-              )}
+          {/* <div className="header_menuitem_wrap "> */}
+          {this.isLogin ? (
             <li className="header_menuitem underline-from-left">
-              <Link to="/signup">
-                <span>회원가입</span>
+              <Link to="/">
+                <sapn>로그아웃</sapn>
               </Link>
             </li>
-          </div>
+          )
+            : (
+              <li className="header_menuitem underline-from-left">
+                <Link to="./login">
+                  <span>로그인</span>
+                </Link>
+              </li>
+            )}
+          <li className="header_menuitem underline-from-left">
+            <Link to="/signup">
+              <span>회원가입</span>
+            </Link>
+          </li>
+          {/* </div> */}
         </ul>
         <ul className="header_menuicon">
           <li className="header_menuicon_item only_mobile">
@@ -91,14 +127,20 @@ class Header extends PureComponent {
               type="button"
               className="header_menuicon_button"
               onClick={onClickEvent}
-            >              <i className="button" />
+            >
+              <i className="button" />
             </button>
           </li>
         </ul>
-        {mode ? <ShowMenu onClick={this.onClickHandlerMenuIcon} onClickEvent={onClickEvent} /> : ' '}
+        {mode ? <ShowMenu onClickEvent={onClickEvent} /> : ' '}
       </header>
     );
   }
 }
-
+Header.propTypes = {
+  onClickEvent: PropTypes.isRequired,
+  mode: PropTypes.string.isRequired,
+  logoOrange: PropTypes.string.isRequired,
+  logoWhite: PropTypes.string.isRequired,
+};
 export default Header;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,13 @@
 import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../../components/header/Header';
+import SearchList from './SearchList';
 import FoodImtem from './FoodItem';
 import FoodDatas from './FoodDatas';
 import Footer from '../../components/footer/Footer';
 import './main.scss';
-import logo from '../../imges/mango_Logo.jpg';
+import logoOrange from '../../imges/logo-orange.png';
+import logoWhite from '../../imges/logo-white.png';
 import logo2 from '../../imges/logo2.svg';
 
 class Main extends PureComponent {
@@ -12,14 +15,14 @@ class Main extends PureComponent {
     super(props);
     this.state = {
       mode: false,
-      value: '',
+      textValue: '',
 
     };
   }
 
   onChangeHandler = (e) => {
     this.setState({
-      value: e.target.value,
+      textValue: e.target.value,
     });
   }
 
@@ -41,19 +44,72 @@ class Main extends PureComponent {
   // };
 
   render() {
-    console.log('온체인지 될때마다 출력!');
+    console.log(this.state.textValue);
     return (
       <div className="main-page">
         {/* {메인 헤더부분} */}
         <Header
-          logo={logo}
+          logoOrange={logoOrange}
+          logoWhite={logoWhite}
           mode={this.state.mode}
           onClickEvent={this.onClickHandlerMenuIcon}
-          onChangeEvent={this.onChangeHandler}
         />
         {/* {메인 리스트부분 } */}
         <main className="main">
-          <article className="main_article">
+          <article className="main_article main_padding">
+            <header className="main_header">
+              <p className="main_header_title">솔릭한 리뷰 믿을 수 있는 평점!</p>
+              <h1 className="main_header_title">WE 플레이트</h1>
+              <fieldset className="main_search">
+                <legend>전체 검색</legend>
+                <label className="search_word" htmlFor="m-s">
+                  <span className="icon">검색 :</span>
+                  <input
+                    type="text"
+                    className="home_searchInput"
+                    id="m-s"
+                    placeholder="지역, 식당 또는 음식"
+                    autoComplete="off"
+                    name="m-s"
+                    onChange={this.onChangeHandler}
+                  />
+                  <span className="clear_btn">CLEAR</span>
+                </label>
+                <input
+                  className="btn_search"
+                  type="submit"
+                  value="검색"
+                />
+              </fieldset>
+              {FoodDatas.map((ele) => (
+                <SearchList
+                  key={ele.id}
+                  url={ele.url}
+                  des={ele.des}
+                />
+              ))}
+              <aside className="shortcut_app type_main">
+                <a className="btn inbound only_desktop" href="/">
+                  <img
+                    src="https://mp-seoul-image-production-s3.mangoplate.com/web/resources/nf58dxcb-7ikpwam.png"
+                    alt="최대 할인 바로 이동"
+                  />
+                </a>
+                <button type="button" className="btn android">
+                  <img
+                    src="https://mp-seoul-image-production-s3.mangoplate.com/web/resources/bzdlmp2toaxrdjqg.png"
+                    alt="android market button"
+                    width="180px"
+                  />
+                </button>
+                <button type="button" className="btn ios">
+                  <img
+                    src="https://mp-seoul-image-production-s3.mangoplate.com/web/resources/f7eokfaszt4gpkh6.svg?v=1"
+                    alt="app store button"
+                  />
+                </button>
+              </aside>
+            </header>
             <section className="main_article_section">
               <div className="tittle_wrap">
                 <div className="tittle">
@@ -63,16 +119,15 @@ class Main extends PureComponent {
               <div className="list_contain top_list">
                 <ul className="item ">
                   {FoodDatas.map((ele) => (
-                    ele.des.includes(this.state.value)
-                      ? (
 
-                        <FoodImtem
-                          key={ele.id}
-                          src={ele.img}
-                          url={ele.url}
-                          descript={ele.des}
-                        />
-                      ) : ''
+
+                    <FoodImtem
+                      key={ele.id}
+                      src={ele.img}
+                      url={ele.url}
+                      descript={ele.des}
+                    />
+
                   ))}
                   {/* {filtering} */}
                 </ul>
