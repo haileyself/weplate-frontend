b55b3b9d44d68522e63cc0b3177c9bdbfa4e6a62
khsi12345 <khsi6237@gmail.com>
2019-09-07 15:16:12 +0900

    kakaomap 세팅은 완료 테스트 예정

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -6,12 +6,21 @@ class Map extends PureComponent {
     this.ref = React.createRef();
   }
 
-  componentDidMount() {
-    console.log(this.ref.current);
-    new window.kakao.maps.Map(this.ref.current, {
+  componentDidMount = async () => {
+    const mapContainer = document.getElementById('map');
+
+    const mapOption = {
       center: new window.kakao.maps.LatLng(37.57413, 127.01757),
-      level: 3,
-    });
+      level: 4,
+    };
+
+    const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+    // console.log(this.ref.current);
+    // new window.kakao.maps.Map(this.ref.current, {
+    //   center: new window.kakao.maps.LatLng(37.57413, 127.01757),
+    //   level: 3,
+    // });
     // const marker = new window.kakao.maps.Marker({
     //   position: new window.kakao.maps.LatLng(37.57084, 126.99353),
     //   map,
@@ -21,8 +30,8 @@ class Map extends PureComponent {
   render() {
     return (
       <>
-        <div className="map_container">
-          <div ref={this.ref} className="map_style" id="map" />
+        <div className="map_container" style={{ width: '400px' }}>
+          <div ref={this.ref} className="map_style" id="map" style={{ width: '100%', height: '350px' }} />
         </div>
       </>
     );
